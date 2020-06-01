fd5fef3d2d32522cb133341081e3ca9c8c3db8e4
AnaghaV <anagha.1512@gmail.com>
2020-06-01 10:04:44 -0500

    using format

---
+++ ex06.py
@@ -0,0 +1,28 @@
+types_of_people = 10
+x = f"There are {types_of_people} types of people."
+binary = "binary"
+do_not = "don't"
+y = f"Those who know {binary} and those who {do_not}." 
+print(x)
+print(y)
+print(f"I said: {x}")
+print(f"I also said: '{y}'")
+hilarious = False
+joke_evaluation = "Isn't that joke so funny?! {}" 
+print(joke_evaluation.format(hilarious))
+w = "This is the left side of..."
+e = "a string with a right side."
+print(w + e)
+
+n=12
+x=f"There are {n} apples"
+temp="juicy"
+q =f"They are very {temp}"
+print(x)
+print(q)
+print(f"I said {x}")
+print(f"I said '{q}'")
+Temp= 11
+new="I didnt say there are {} apples"
+print(new.format(Temp))
+
