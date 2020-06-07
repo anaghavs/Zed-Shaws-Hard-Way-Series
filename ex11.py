b7ae36746b62896a37951f0f2574b4669ced8c72
AnaghaV <anagha.1512@gmail.com>
2020-06-06 22:47:17 -0500

    more usage of end

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?", end=' ') #Weput an end=' ' at the end of each print line.
+#This tells print tonot end the line with a newline character and go to the next line.
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you weigh?", end=' ')
+weight = input()
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
+print('goodmorning')
