b9e0b53dd069852487c569656b0a665fcc2102db
AnaghaV <anagha.1512@gmail.com>
2020-06-07 22:12:21 -0500

    prompting

---
+++ ex12.py
@@ -0,0 +1,6 @@
+age = input("How old are you? ")
+#This prompts the user with “how old areyou?” and puts the result into the variable.
+#This is how you ask someone a question and get the answer.
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
